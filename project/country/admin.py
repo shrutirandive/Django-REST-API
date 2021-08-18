d30d2e809263c8c1c30bb20507a0f16acc0e2b16
Shruti Randive <54211989+shrutirandive@users.noreply.github.com>
2021-08-18 18:22:58 +0530

    Delete project/country directory

--- project/country/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models.country import Country
-
-# Register your models here.
-class AdminCountry(admin.ModelAdmin):
-    list_display=['name', 'code', 'capital', 'population','id']
-
-admin.site.register(Country, AdminCountry)
